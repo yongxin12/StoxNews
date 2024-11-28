3fe19f6b4588f4665bde057a2579f710c24e5084
Rex <yongxin1@ualberta.ca>
2024-11-28 12:50:42 -0700

    wordcloud added

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -15,6 +15,7 @@ export class Home {
         this.dynamicPlaceholder = new DynamicPlaceholder('home-stock-input');
         this.dynamicPlaceholder.init();
         this.initializeEventListeners();
+        this.initializeWordCloud();
 
         // Assign this instance to the static property
         Home.instance = this;
@@ -40,4 +41,56 @@ export class Home {
             }
         });
     }
+
+    initializeWordCloud() {
+        const keywords = [
+            { text: 'TSLA', size: 40 },
+            { text: 'MSFT', size: 30 },
+            { text: 'GOOGL', size: 35 },
+            { text: 'NVDA', size: 50 },
+            { text: 'AMZN', size: 25 },
+            { text: 'NFLX', size: 20 },
+            { text: 'META', size: 30 }
+        ];
+
+        const width = document.getElementById('word-cloud-container').offsetWidth;
+        const height = 300;
+
+        const svg = d3.select('#word-cloud-container')
+            .append('svg')
+            .attr('width', width)
+            .attr('height', height);
+
+        const layout = d3.layout.cloud()
+            .size([width, height])
+            .words(keywords.map(d => Object.create(d)))
+            .padding(5)
+            .rotate(() => (~~(Math.random() * 2) * 90)) // Randomly rotate
+            .fontSize(d => d.size)
+            .on('end', draw.bind(this));
+
+        console.log('Home instance initialized');
+        layout.start();
+
+        function draw(words) {
+            svg.append('g')
+                .attr('transform', `translate(${width / 2}, ${height / 2})`)
+                .selectAll('text')
+                .data(words)
+                .enter()
+                .append('text')
+                .style('font-size', d => `${d.size}px`)
+                .style('fill', () => `hsl(${Math.random() * 360}, 100%, 50%)`) // Random color
+                .attr('text-anchor', 'middle')
+                .attr('transform', d => `translate(${[d.x, d.y]})rotate(${d.rotate})`)
+                .text(d => d.text)
+                .on('click', d => {
+                    this.stockInput.value = d.text; // Fill input with the selected keyword
+                    console.log(d.text)
+                    this.searchButton.click(); // Trigger search
+                });
+        }
+        
+    }
+    
 }
